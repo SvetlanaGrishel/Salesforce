4ad156324c1e7a612defe9b47a05be39f23accf2
SvetlanaGrishel <27svetlana5869@mail.ru>
2024-12-26 15:47:38 +0300

    SF-3: Added properties, maven.yml updated

--- src/test/java/steps/LoginStep.java
+++ src/test/java/steps/LoginStep.java
@@ -3,11 +3,15 @@ package steps;
 import lombok.extern.log4j.Log4j2;
 import org.openqa.selenium.WebDriver;
 import pages.LoginPage;
+import utils.PropertyReader;
 
 @Log4j2
 public class LoginStep {
 
     LoginPage loginPage;
+
+    private String SF_USERNAME = System.getProperty("SF_USERNAME", PropertyReader.getProperty("SF_USERNAME"));
+    private String SF_PASSWORD = System.getProperty("SF_PASSWORD", PropertyReader.getProperty("SF_PASSWORD"));
 
     public LoginStep(WebDriver driver) {
         loginPage = new LoginPage(driver);
@@ -17,7 +21,7 @@ public class LoginStep {
         log.info("Login to the 'Salesforce' system with credentials");
         loginPage.open()
                 .isPageOpened()
-                .login("tborodich@tms.sandbox", "Password001")
+                .login(SF_USERNAME, SF_PASSWORD)
                 .open()
                 .isPageOpened();
     }
